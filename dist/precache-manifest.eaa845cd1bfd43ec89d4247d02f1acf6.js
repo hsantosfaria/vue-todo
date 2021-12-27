ece938caf2718fe43ebdbe8117e8f6ea4be048fd
Helmar Santos Faria <hfaria@mercadocar.com.br>
2021-12-27 20:49:43 -0300

    first commit

---
+++ dist/precache-manifest.eaa845cd1bfd43ec89d4247d02f1acf6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d548fc474b0025a2aa35",
+    "url": "/css/chunk-17bc2355.c479b06d.css"
+  },
+  {
+    "revision": "23462f549fa8d645126d",
+    "url": "/css/chunk-vendors.2973f4f6.css"
+  },
+  {
+    "revision": "7261de5f5950ca2d2d1094ad0ff8f73c",
+    "url": "/img/fundo.7261de5f.webp"
+  },
+  {
+    "revision": "110cd46facc5aa16aee2942c3ab3a87d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e609c56c0a0fa2b70714",
+    "url": "/js/app.6a829282.js"
+  },
+  {
+    "revision": "d548fc474b0025a2aa35",
+    "url": "/js/chunk-17bc2355.88b3d9a1.js"
+  },
+  {
+    "revision": "aafc9b9dc99407bd60bc",
+    "url": "/js/chunk-2d21d18e.22083b34.js"
+  },
+  {
+    "revision": "23462f549fa8d645126d",
+    "url": "/js/chunk-vendors.f26489cc.js"
+  },
+  {
+    "revision": "fbafddbca0454e2cf83c4640f9391b35",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
